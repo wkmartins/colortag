7e87de8ee0ffd3aa8afcd3b8965d73fbda7a7bbd
wagnerkaue <121360920+wagnerkaue@users.noreply.github.com>
2023-05-13 18:53:53 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Python library for adding ANSI colors to the terminal output using a simple syntax'
+
+setup(
+    name="colortag",
+    version=VERSION,
+    author="Wagner Kauê",
+    author_email="<wkmartinst@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'ansi', 'colors', 'color', 'terminal'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
